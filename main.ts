fcf463717ac8290da0658b39f4c1b183026f74a2
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-11-24 14:04:06 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    naiara.move(-1)
+    jokalaria.move(-1)
 })
 input.onButtonPressed(Button.B, function () {
-    naiara.move(1)
+    jokalaria.move(1)
 })
-let naiara: game.LedSprite = null
-naiara = game.createSprite(2, 4)
+let meteorito: game.LedSprite = null
+let jokalaria: game.LedSprite = null
+jokalaria = game.createSprite(2, 4)
 basic.forever(function () {
-	
+    basic.pause(randint(1000, 1500))
+    meteorito = game.createSprite(randint(0, 4), 0)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(200)
+    }
+    if (meteorito.isTouching(jokalaria)) {
+        game.gameOver()
+    }
+    basic.pause(200)
 })
