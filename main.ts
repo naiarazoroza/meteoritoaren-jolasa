07a1e674065711ed1f1cd02a97d6cc706a9ae9d5
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-11-24 13:53:53 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    naiara.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    naiara.move(1)
+})
+let naiara: game.LedSprite = null
+naiara = game.createSprite(2, 4)
+basic.forever(function () {
+	
+})
